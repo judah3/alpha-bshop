cffef2acb3583b52b6945f7d070fade455275b7a
Jude Christian Agagad <jude@boomsroucing.com>
2022-08-12 04:33:59 +0800

    Configure app for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn beeshop.wsgi
--- beeshop/settings.py
+++ beeshop/settings.py
@@ -1,3 +1,9 @@
+import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 """
 Django settings for beeshop project.
 
@@ -59,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'beeshop.urls'
@@ -150,3 +157,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+
+django_heroku.settings(locals())
